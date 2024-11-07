2b302952c04003cc21ddef4b1f83a6164e459958
Breno L. Lopes <brernolopes@hotmail.com>
2024-11-07 13:14:19 -0300

    :sparkles: feat: criando  componente de hamburger

---
+++ src/app/componentes/Hamburger.tsx
@@ -0,0 +1,19 @@
+interface HamburgerProps {
+  show: boolean;
+}
+
+
+export default function Hamburger({show}: HamburgerProps) {
+  return (
+    <div>
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+      </ol>
+    </div>
+  )
+};
